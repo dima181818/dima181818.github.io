46993b3cd8770aaa9c3bd4ab27f42aefc883cf5f
dima181818 <47688702+dima181818@users.noreply.github.com>
2022-01-25 01:06:03 +0300

    Add files via upload

--- JS.js
+++ JS.js
@@ -4,14 +4,11 @@ let ticking = false;
 $(document).ready(function() {
     $(".wrapper").on("click", "a", function(event) {
       event.preventDefault();
-      //забираем идентификатор бока с атрибута href
       var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
         top = $(id).offset().top - document.documentElement.clientHeight/3;
-      //анимируем переход на расстояние - top за 1000 мс
       $('body,html').animate({
         scrollTop: top
-      }, 1000);
+      }, 750);
     });
   });
 
@@ -19,8 +16,14 @@ function scrollfunction(scrollPos) {
 
     for(var i = 0; i < document.getElementsByClassName("spanline").length; i++)
     {
-        if(document.getElementsByClassName("spanline")[i].style.opacity = "0" && document.getElementsByClassName("spanline")[i].getBoundingClientRect().top < 4 / 5 * document.documentElement.clientHeight)
+        if(document.getElementsByClassName("spanline")[i].style.opacity = "0" &&
+         document.getElementsByClassName("spanline")[i].getBoundingClientRect().top < 4 / 5 * document.documentElement.clientHeight &&
+         document.getElementsByClassName("spanline")[i].getBoundingClientRect().top > 0)
         document.getElementsByClassName("spanline")[i].style.opacity = "1"
+        else if(document.getElementsByClassName("spanline")[i].style.opacity = "1" &&
+         ( document.getElementsByClassName("spanline")[i].getBoundingClientRect().top > 4 / 5 * document.documentElement.clientHeight ||
+         document.getElementsByClassName("spanline")[i].getBoundingClientRect().top < 0))
+        document.getElementsByClassName("spanline")[i].style.opacity = "0"
     }
 
     if(scrollPos > 550)
